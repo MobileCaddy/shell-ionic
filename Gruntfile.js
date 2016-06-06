bd68029426b903fa2928bde8ae488e89cfadfffd
Todd Halfpenny <todd@mobilecaddy.net>
2016-06-06 10:18:57 +0100

    ISHL-25 Fixed angular-mocks version and corrected typo in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                 'www/js/app.js',
                 'www/js/**/*.js',],
       test: [
-        'tests/**/*.js'
+        'tests/**/*.js',
         '!tests/coverage/**/*.*'
         ]
     },
